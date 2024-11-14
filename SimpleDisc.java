3132b5ecc8adb9751290e9eb2c8354f085fd1139
sharonelbo <82945364+sharonelbo@users.noreply.github.com>
2024-11-14 12:32:54 +0200

    First commit
    Adding a new class for simple disc

---
+++ SimpleDisc.java
@@ -0,0 +1,17 @@
+public class SimpleDisc implements Disc{
+
+    @Override
+    public Player getOwner() {
+        return null;
+    }
+
+    @Override
+    public void setOwner(Player player) {
+
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+}
